e9fc9dd55dc9a10337f83ea0f6e9239a5823d71b
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2018-10-10 09:45:04 -0500

    Js con animación básica.

---
+++ js/funcionalidades.js
@@ -0,0 +1,19 @@
+/**
+ * Autor: Camilo Figueroa ( Crivera )
+ * Este es un sencillo ejemplo para probar un framework de animaciones en html css.
+ * Ejemplo: https://www.supremo.co.uk/
+ */
+
+
+window.onload = function()
+{
+    var contenedor1 = document.getElementById( "contenedor-texto" );
+
+    TweenMax.to( contenedor1, 10, { left: "100px" } );
+
+}
+
+
+
+
+
